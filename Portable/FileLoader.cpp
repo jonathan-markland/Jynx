3aa010e47721c3b4b04097fbc87c8830b6f70881
jonathan <jynx_emulator@yahoo.com>
2014-08-24 21:16:30 +0100

    Copyright comment removed from source code, and readme.htm updated to include links to GitHub.

--- Portable/FileLoader.cpp
+++ Portable/FileLoader.cpp
@@ -18,12 +18,6 @@
 //		jynx_emulator {at} yahoo {dot} com
 // 
 
-//
-// Camputers 48K/96K Lynx emulator.
-// Software designed and developed by Jonathan Markland.
-// (C) 2014 Jonathan Markland
-//
-
 #include "FileLoader.h"
 
 namespace Jynx
